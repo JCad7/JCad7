518a697e27e780af7b45af4d6507091507d54f2d
Julian <159921537+JCad7@users.noreply.github.com>
2024-03-20 09:09:03 -0400

    Update 1.6 Logical Operators.js

--- code/Unit 1/1.6 Logical Operators.js
+++ code/Unit 1/1.6 Logical Operators.js
@@ -1,27 +1,24 @@
 const prompt = require("prompt-sync")();
 
-let ans1 = prompt("Do you want to play a game? (y/n): ")
+let ans1 = prompt("Do you want to play a game? (y/n): ").toLowerCase()
 
-if (ans1 == "y") {
+if (ans1 == "y" || ans1 == "yes") {
     console.log("You have selected yes.")
 
-    let num1 = prompt("Enter a number: ")
-        num1 = parseInt(num1)
+    let num1 = parseInt(prompt("Enter a number: "))
         if (num1 > 10 && num1 <50) {
             console.log("Your number should be greater than 50. ") 
         }
 
 
-    let num2 = prompt("Enter another number: ")
-        num2 = parseInt(num2)
+    let num2 = parseInt(prompt("Enter another number: "))
         if (num2 % 2 == 0) {
             console.log("Your next number should be odd")
         } else if (num2 % 2 == 1) {
             console.log("Your next number should be")
         }
-    let num3 = prompt("Enter a third and final number: ")
-        num3 = parseInt(num3)
-        if (num1 + num2 + num3 + 1 % 10 == 0) {
+    let num3 = parseInt(prompt("Enter a third and final number: "))
+        if ((num1 + num2 + num3 + 1) % 10 == 0) {
         console.log("Your numbers are Squirrelly ")
         } else {
         console.log("Your numbers are not Squirrelly ")
@@ -33,7 +30,7 @@ if (ans1 == "y") {
         console.log("Your numbers are not Jazzy")
     }
 
-} else if (ans1 == "n") {
+} else if (ans1 == "n" || ans1 == "no") {
     console.log("Ok bye")
 } else {
     console.log("That's not 'y' or 'n'.")
